d3281d6c938baef2ee615d6a0e3df306ca766c4f
Vitalij V <38693036+v0lkas@users.noreply.github.com>
2018-04-25 13:46:14 +0300

    Git test

--- tests/tst01.java
+++ tests/tst01.java
@@ -4,5 +4,7 @@ public class tst01 {
     public static void main(String[] args) {
         byte test = 100;
         System.out.println(test);
+        int test2 = 1000;
+        System.out.println(test2);
     }
 }
